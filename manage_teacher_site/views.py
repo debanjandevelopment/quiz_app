09b79468d3e28f2faf4cc4ce9e93abdce3996eb1
Debanjan Paul <70275844+paulDebanjan@users.noreply.github.com>
2024-02-18 19:49:33 +0600

    v1.61
    database worked

--- manage_teacher_site/views.py
+++ manage_teacher_site/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.views.generic.edit import CreateView,UpdateView, DeleteView
 from .models import CreateQuestionPaper
+from django.urls import reverse,reverse_lazy
 
 def dashboard(request):
     return render (request,'dashboard.html')
@@ -10,5 +11,15 @@ class CreateQuestionPaperView(CreateView):
     fields= ['question_paper_name','total_question','exam_time','subject']
     template_name="form.html"
     
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['action'] = "Create"
+        context['form_title'] = "Exam Paper"
+        return context
+
     def form_valid(self, form):
+        print('receive')
         return super().form_valid(form)
+    
+    def get_success_url(self, *args, **kwargs):
+        return reverse_lazy('teacher_manage_app:teacher_dashboard')
